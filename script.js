bcb46effef9dd626a2dcfb8b59809e9189249bba
wallacelt25 <114073349+wallacelt25@users.noreply.github.com>
2022-12-21 00:13:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//Please put your answer here
+$(function() {
+
+  // Signup modal
+
+  $('.signup-show').click(function() {
+    $('#signup-modal').fadeIn();
+  });
+
+  $('#close-modal').click(function() {
+    $('#signup-modal').fadeOut();
+  });
+
+});
